3caf4a9162c99bb2d5247a23e55064a3368e56a0
kgremillion <kgremillion19@gmail.com>
2020-02-21 12:42:00 -0600

    Updated Python Homework

--- PyElections/main.py
+++ PyElections/main.py
@@ -1,1 +1,14 @@
+import os
+import csv
 
+with open('houston_election_data.csv', newline="") as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=',')
+       # for row in csvreader:
+           # print(', '.join(row))
+           print(csvreader)
+
+name = str(houston_election_data[0])
+county = str(houston_election_data[1])
+vote = int(houston_data[2])
+
+total_votes = sum(vote)
