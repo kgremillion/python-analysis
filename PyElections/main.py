3f15c6a4566d0114e415a5592e8ada4e6e8065aa
kgremillion <kgremillion19@gmail.com>
2020-02-22 17:24:28 -0600

    Final Push for python-challenge

--- PyElections/main.py
+++ PyElections/main.py
@@ -1,14 +1,61 @@
 import os
 import csv
 
-with open('houston_election_data.csv', newline="") as csvfile:
-        csvreader = csv.DictReader(csvfile, delimiter=',')
-       # for row in csvreader:
-           # print(', '.join(row))
-           print(csvreader)
+total_votes = 0
+candidates = {}
+candidate_votes = {}
+vote_percent ={}
+winner_votes = 0
+winner = ""
+filename = ""
 
-name = str(houston_election_data[0])
-county = str(houston_election_data[1])
-vote = int(houston_data[2])
+with open('houston_election_data.csv','r', encoding="utf-8-sig") as csvfile:
+    
+    csvreader = csv.reader(csvfile)
+    csv_header = next(csvfile)
+    
+    for row in csvreader:
+        
+        total_votes += 1
+        candidates = row[0]
+        if candidates in candidate_votes:
+            candidate_votes[candidates] = candidate_votes[candidates] + 1
+        else:
+            candidate_votes[candidates] = 1
 
-total_votes = sum(vote)
+for can, vote_count in candidate_votes.items():
+    vote_percent[can] = "{:.0%}".format(vote_count / total_votes)
+
+import operator
+candidate_votes
+sorted_votes = dict( sorted(candidate_votes.items(), key=operator.itemgetter(1),reverse=True))
+
+top_candidates = dict(sorted(sorted_votes.items(), key=operator.itemgetter(1), reverse=True)[:2])
+
+print("------------------------------------------")
+print("Election Results")
+print("----------------------------------------")
+print(f"Total Votes: {total_votes}")
+print("-----------------------------------------")
+for can, vote_count in candidate_votes.items():
+    print(f"{can}: {vote_percent[can]} ({vote_count})")
+print("------------------------------------------")
+print(f"1st Advancing Candidate : Sylvester Turner")
+print(f"2nd Advancing Candidate : Tony Buzbee")
+print("------------------------------------------")
+
+save_file = filename + "election_results.txt"
+filepath = os.path.join(".", save_file)
+with open(filepath,"w", encoding="utf-8") as fh:
+    fh.write("---------------------------------------\n")
+    fh.write("Election Results\n")
+    fh.write("---------------------------------------\n")
+    fh.write(f"Total Votes: {total_votes}" + "\n")
+    fh.write("---------------------------------------\n")
+    for can, vote_count in candidate_votes.items():
+       fh.write(f"{can}: {vote_percent[can]} ({vote_count})" + "\n")
+    fh.write("---------------------------------------\n")
+    fh.write(f"1st Advancing Candidate : Sylvester Turner" + "\n")
+    fh.write(f"2nd Advancing Candidate : Tony Buzbee" + "\n")
+    fh.write("----------------------------------------\n")
+
--- PyFinances/main.py
+++ PyFinances/main.py
@@ -1,1 +1,60 @@
+import os
+import csv
 
+with open('budget_data.csv','r', encoding="utf-8-sig") as csvfile:
+
+    csvreader = csv.reader(csvfile)
+    csv_header = next(csvfile)
+    
+    profit_loss = []
+    profit_loss_total = 0
+    date = []
+    rev_total = 0
+    avg_change = 0
+    change_list = []
+    filename = ""
+    
+    for row in csvreader:
+        profit_loss.append(int(row[0]))
+        date.append(row[1])
+
+    total_months = (len(date))
+    rev_total = sum(profit_loss)
+
+    for i in range(len(profit_loss)-1):
+        change_total = profit_loss[i + 1] - profit_loss[i]
+        avg_change = avg_change + change_total
+        
+        change_list.append(change_total)
+
+    total_change = avg_change/ (len(profit_loss)-1)
+
+    change_rounded = (round(total_change, 2))
+
+    max_change = max(change_list)
+    max_month = change_list.index(max(change_list)) + 1
+    min_change = min(change_list)
+    min_month = change_list.index(min(change_list)) + 1
+
+    print("---------------------------------------------------")
+    print("Financial Analysis")
+    print("---------------------------------------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total: ${rev_total}")
+    print(f"Average Change: ${change_rounded}")
+    print(f"Greatest Increase in Profits: {date[max_month]} ${max_change}")
+    print(f"Greatest Decrease in Profits: {date[min_month]} ${min_change}")
+    print("---------------------------------------------------")
+
+    save_file = filename + "financial_analysis.txt"
+    filepath = os.path.join(".", save_file)
+    with open(filepath,"w", encoding="utf-8") as fh:
+        fh.write("---------------------------------------------------\n")
+        fh.write("Financial Analysis\n")
+        fh.write("---------------------------------------------------\n")
+        fh.write(f"Total Months: {total_months}\n")
+        fh.write(f"Total: ${rev_total}\n")
+        fh.write(f"Average Change: ${change_rounded}\n")
+        fh.write(f"Greatest Increase in Profits: {date[max_month]} ${max_change}\n")
+        fh.write(f"Greatest Decrease in Profits: {date[min_month]} ${min_change}\n")
+        fh.write("---------------------------------------------------\n")
